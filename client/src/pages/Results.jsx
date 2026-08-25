220059c0b9a74aef2b7257620bfaccb3dfa5dd02
Md Faizan <mdfaizan7707@gmail.com>
2026-08-25 19:17:18 +0530

    Change in UI

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -216,6 +216,8 @@ const QuestionReviewCard = ({ q, index }) => {
   )
 }
 
+// Main Page
+
 export default function Results() {
   const { sessionId } = useParams()
   const navigate      = useNavigate()
